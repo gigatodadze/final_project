c5aa837f61c52e7b241900e0316b0a6b4b8b44ee
gigatodadze <72299807+gigatodadze@users.noreply.github.com>
2021-10-16 20:31:45 +0400

    Quiz 2 mere kide vecdebi gavauketeso :D

--- src/test/java/Quiz2Test.java
+++ src/test/java/Quiz2Test.java
@@ -1,8 +1,12 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.nio.file.LinkPermission;
 import java.util.List;
@@ -22,21 +26,72 @@ public class Quiz2Test {
         WebElement startButton = driver.findElement(By.id("startStopButton"));
         startButton.click();
 
-        while (!driver.findElement(By.className("progress-bar")).getAttribute("aria-valuenow").equals("100")){
-            driver.manage().timeouts().implicitlyWait(2, TimeUnit.MILLISECONDS);
-         }
-        System.out.print("'100%'");
+        // ეს ჩემი პრო მიგნება იყო მაგრამ მგონი ასე არ უნდა ვქნათ. რახან მაინცდამაინც ვებდრაივერით გვინდა დავუცდაოთ
+//        while (!driver.findElement(By.className("progress-bar")).getAttribute("aria-valuenow").equals("100")){
+//            driver.manage().timeouts().implicitlyWait(2, TimeUnit.MILLISECONDS);
+//         }
+//        System.out.print("'100%'");
 
-        driver.get("http://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
-        WebElement option1 = driver.findElement(By.xpath("//*[@id=\"dropdowm-menu-1\"]/option[2]"));
-        option1.click();
+        WebElement progressBar = driver.findElement(By.className("progress-bar"));
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.attributeContains(progressBar, "aria-valuenow", "100"));
+        System.out.println("'100%'");
+        driver.quit();
+    }
 
-//        List<WebElement> checkboxes = driver.findElements(By.tagName("label"));
-//        for (int i=0; i<checkboxes.size(); i++){
-//            if(!checkboxes[i].isSelected())
-//                checkbox.click();
-//        }
+    @Test
+    public void secondTest() {
 
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("http://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+        WebElement LanguageDropDown = driver.findElement(By.id("dropdowm-menu-1"));
+        Select myLang = new Select(LanguageDropDown);
+        myLang.selectByVisibleText("C#");
+        String selected = myLang.getFirstSelectedOption().getText();
 
+        if(selected.equals("C#")){
+           System.out.println("Selected programming language "+ selected + " matches to C#");
+        } else {
+            System.out.println("Selected programming language doesn't match " + selected);
+        }
+
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector("#checkboxes input[type=checkbox]"));
+        for (WebElement checkbox : checkBoxes) {
+            if (!checkbox.isSelected()) {
+                checkbox.click();
+            }
+        }
+
+        WebElement myRadio = driver.findElement(By.cssSelector("#radio-buttons > input[type=radio][value=yellow]"));
+        myRadio.click();
+
+        Select fruitDropdown = new Select(driver.findElement(By.id("fruit-selects")));
+        List<WebElement> fruits = fruitDropdown.getOptions();
+        for (WebElement fruit : fruits) {
+            if (fruit.getAttribute("value").equals("orange") && !fruit.isEnabled()) {
+                System.out.println(" 'Orange' option in dropdown is disabled");
+            }
+        }
+        driver.quit();
+    }
+    @Test
+    public void thirdTask(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("http://the-internet.herokuapp.com/iframe");
+        WebElement iframe = driver.findElement(By.id("mce_0_ifr"));
+        driver.switchTo().frame(iframe);
+        WebElement body = driver.findElement(By.tagName("body"));
+        body.click();
+        WebElement textArea = driver.findElement(By.xpath("//*[@id=\"tinymce\"]/p"));
+        textArea.clear();
+        textArea.sendKeys("Here Goes");
+        driver.switchTo().defaultContent();
+        WebElement ButtonCen = driver.findElement(By.cssSelector("button[aria-label=\"Align center\"]"));
+        ButtonCen.click();
+        driver.quit();
     }
 }
