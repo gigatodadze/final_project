282cd86db4b11ae1e438cc68cf17aead789b555a
gigatodadze <72299807+gigatodadze@users.noreply.github.com>
2021-11-23 20:28:33 +0400

    removed unused imports

--- src/test/java/SelenideBasics2Test.java
+++ src/test/java/SelenideBasics2Test.java
@@ -1,34 +1,12 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import static com.codeborne.selenide.AssertionMode.SOFT;
-import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.*;
-import com.codeborne.selenide.testng.SoftAsserts;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
-import java.io.FileNotFoundException;
-import java.time.Duration;
 import java.util.List;
-
-import static com.codeborne.selenide.CollectionCondition.*;
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Configuration.*;
-
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideBasics2Test {
 
